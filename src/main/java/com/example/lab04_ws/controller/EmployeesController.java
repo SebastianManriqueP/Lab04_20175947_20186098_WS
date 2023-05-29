3a0492809560bde099363a0e314e879aa68cef7e
Sebastian Manrique <a2018609@pucp.edu.pe>
2023-05-28 20:46:50 -0500

    buscar Employee por id

--- src/main/java/com/example/lab04_ws/controller/EmployeesController.java
+++ src/main/java/com/example/lab04_ws/controller/EmployeesController.java
@@ -5,9 +5,11 @@ import com.example.lab04_ws.repository.EmployeesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/employees")
@@ -17,4 +19,8 @@ public class EmployeesController {
 
     @GetMapping("/listar")
     List<Employees> listarEmployees(){return employeesRepository.findAll();}
+
+    @GetMapping("/buscar")
+    Optional<Employees> buscarEmployees(@RequestParam("id") Integer id){
+        return employeesRepository.findById(id);}
 }
