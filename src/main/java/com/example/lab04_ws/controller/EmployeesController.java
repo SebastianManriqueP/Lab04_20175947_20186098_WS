73e50c5989ebbf47a841b8f1f3892ee073ab0acf
FrankLaquise <a20175947@pucp.edu.pe>
2023-05-29 23:16:50 -0500

    empl

--- src/main/java/com/example/lab04_ws/controller/EmployeesController.java
+++ src/main/java/com/example/lab04_ws/controller/EmployeesController.java
@@ -3,6 +3,7 @@ package com.example.lab04_ws.controller;
 import com.example.lab04_ws.entities.Employees;
 import com.example.lab04_ws.repository.EmployeesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.core.io.ByteArrayResource;
 
+
+import java.lang.StringBuilder;
 @RestController
 @RequestMapping("/employees")
 public class EmployeesController {
@@ -20,7 +24,23 @@ public class EmployeesController {
     @GetMapping("/listar")
     List<Employees> listarEmployees(){return employeesRepository.findAll();}
 
+    @GetMapping("/descargarListaTrabajadores")
+    public ResponseEntity<String> descargarListaTrabajadores() {
+        List<Employees> listaTrabajadores = employeesRepository.findAllExceptPresident();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Employees employee : listaTrabajadores) {
+            stringBuilder.append(employee.toString()).append("\n");
+        }
+        String content = stringBuilder.toString();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        headers.setContentDisposition(ContentDisposition.parse("attachment; filename=listaDeTrabajadores.txt"));
+
+        return ResponseEntity.ok().headers(headers).body(content);
+    }
     @GetMapping("/buscar")
     Optional<Employees> buscarEmployees(@RequestParam("id") Integer id){
         return employeesRepository.findById(id);}
+
 }
--- src/main/java/com/example/lab04_ws/repository/EmployeesRepository.java
+++ src/main/java/com/example/lab04_ws/repository/EmployeesRepository.java
@@ -2,6 +2,12 @@ package com.example.lab04_ws.repository;
 
 import com.example.lab04_ws.entities.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM employees i join jobs z on i.job_id=z.job_id where NOT z.job_id='AD_PRES';")
+    List<Employees> findAllExceptPresident();
 }
